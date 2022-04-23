f2f83ddc234b994e8c8e9d1285a065764085d1b4
Isabelle-Vicente <73740552+Isabelle-Vicente@users.noreply.github.com>
2022-04-23 18:12:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Meu primeiro site em PHP para o bootcamp</title>
+</head>
+<body>
+	<?php
+	for ( $i = 0; $i < 10 ; $i++) {
+		print("Linha númro " . $i . "<br/>"  );
+	} 
+	 ?>
+
+</body>
+</html>
